e739b58bc034a88d69d75e69272a6ea4d904215d
SimonJohanning <Simonjohanning@gmail.com>
2025-03-01 00:24:09 +0100

    changing code base to work with sql database

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import dbUtils
 import sqlite_to_xml
 import util
+import dbhandling
 
-sqlite_to_xml.generate_xml_data('./openthesaurus_dump_250224.sql')
+# establish the db
+cursor, connection = dbhandling.import_sql_dump(
+    host="localhost",
+    user="sim",
+    password="developmentPassw0rd",
+    database="open_thesaurus",
+    dump_file_path="openthesaurus_dump.sql",
+    quiet=True
+)
+
+#print(dbUtils.findSynsetForWord(connection, 'Artilleriebeschuss'))
+sqlite_to_xml.generate_xml_data(connection)
+
+#sqlite_to_xml.generate_xml_data('./openthesaurus_dump_250224.sql')
 #util.extract_tarball('./openthesaurus_dump_250224.tar.bz2')
